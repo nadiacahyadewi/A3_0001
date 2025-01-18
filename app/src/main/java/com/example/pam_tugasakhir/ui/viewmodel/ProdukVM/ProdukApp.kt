17fc44f48dd03f1f2640a67f2a120b96dfd38df0
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-18 23:24:18 +0700

    membuat ProdukApp

---
+++ app/src/main/java/com/example/pam_tugasakhir/ui/viewmodel/ProdukVM/ProdukApp.kt
@@ -0,0 +1,31 @@
+package com.example.pam_tugasakhir.ui.viewmodel.ProdukVM
+
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.pam_tugasakhir.ui.navigation.PengelolaHalaman
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProdukApp(modifier: Modifier = Modifier) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        //topBar = {TopAppBar(scrollBehavior = scrollBehavior)}
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            PengelolaHalaman()
+        }
+    }
+}
