7cdfb58929d484cd2b55c6fe47fac4a29936f69f
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-18 17:25:52 +0700

    membuat ProdukRepository

---
+++ app/src/main/java/com/example/pam_tugasakhir/repository/ProdukRepository.kt
@@ -0,0 +1,51 @@
+package com.example.pam_tugasakhir.repository
+
+import com.example.pam_tugasakhir.model.Produk
+import com.example.pam_tugasakhir.service.ProdukService
+import okio.IOException
+
+interface ProdukRepository {
+    suspend fun insertProduk(produk: Produk)
+
+    suspend fun getProduk(): List<Produk>
+
+    suspend fun updateProduk(idProduk: String, produk: Produk)
+
+    suspend fun deleteProduk(idProduk: String)
+
+    suspend fun getProdukById(idProduk: String): Produk
+}
+
+class NetworkProdukRepository(
+    private val produkApiService: ProdukService
+) : ProdukRepository {
+    override suspend fun insertProduk(produk: Produk) {
+        produkApiService.insertProduk(produk)
+    }
+
+    override suspend fun updateProduk(idProduk: String, produk: Produk) {
+        produkApiService.updateProduk(idProduk, produk)
+    }
+
+    override suspend fun deleteProduk(idProduk: String) {
+        try {
+            val response = produkApiService.deleteProduk(idProduk)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete produk. HTTP Status Code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getProduk(): List<Produk> =
+        produkApiService.getAllProduk()
+
+    override suspend fun getProdukById(idProduk: String): Produk {
+        return produkApiService.getProdukById(idProduk)
+    }
+}
