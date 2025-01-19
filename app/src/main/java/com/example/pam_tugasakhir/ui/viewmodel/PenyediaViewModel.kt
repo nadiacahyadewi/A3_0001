55c147013a8a7e44ac271b4ebc454fd80066a02d
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-19 10:14:58 +0700

    membuat PenyediaViewModel untuk insert

--- app/src/main/java/com/example/pam_tugasakhir/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam_tugasakhir/ui/viewmodel/PenyediaViewModel.kt
@@ -6,10 +6,13 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pam_tugasakhir.ProdukApplications
 import com.example.pam_tugasakhir.ui.viewmodel.ProdukVM.ProdukHomeVM
+import com.example.pam_tugasakhir.ui.viewmodel.produk.ProdukInsertVM
+
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { ProdukHomeVM(aplikasiproduk().container.produkRepository) }
+        initializer { ProdukInsertVM(aplikasiproduk().container.produkRepository) }
     }
 }
 
