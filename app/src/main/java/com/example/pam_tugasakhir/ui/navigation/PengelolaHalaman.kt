c78c35343579ee35c79cae1cfca50720ad8269e1
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-19 14:39:05 +0700

    menambahkan Produk Detail di PengelolaHalaman

--- app/src/main/java/com/example/pam_tugasakhir/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_tugasakhir/ui/navigation/PengelolaHalaman.kt
@@ -3,11 +3,15 @@ package com.example.pam_tugasakhir.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pam_tugasakhir.ui.view.produk.DestinasiDetail
 import com.example.pam_tugasakhir.ui.view.produk.DestinasiEntry
 import com.example.pam_tugasakhir.ui.view.produk.DestinasiHome
+import com.example.pam_tugasakhir.ui.view.produk.DetailScreen
 import com.example.pam_tugasakhir.ui.view.produk.EntryProdukScreen
 import com.example.pam_tugasakhir.ui.view.produk.HomeScreen
 
@@ -23,7 +27,9 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                 navigateToItemEntry = {
                     navController.navigate(DestinasiEntry.route)
                 },
-                onDetailClick = {}
+                onDetailClick = { idProduk ->  // Handle onDetailClick here
+                    navController.navigate("${DestinasiDetail.route}/$idProduk")
+                }
             )
         }
 
@@ -35,6 +41,21 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                             inclusive = true
                         }
                     }
+                }
+            )
+        }
+
+        // Detail Screen
+        composable(
+            route = "${DestinasiDetail.route}/{idProduk}",
+            arguments = listOf(navArgument("idProduk") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val idProduk = backStackEntry.arguments?.getString("idProduk") ?: ""
+            DetailScreen(
+                idProduk = idProduk,
+                onNavigateBack = { navController.navigateUp() },
+                onEditClick = {
+                    navController.navigate("${DestinasiEntry.route}?idProduk=$idProduk")
                 }
             )
         }
