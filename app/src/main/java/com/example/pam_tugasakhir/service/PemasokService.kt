548a98acf38999911cc9b4263f87fd391182d396
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-20 23:22:02 +0700

    menambahkan Pemasok Service

---
+++ app/src/main/java/com/example/pam_tugasakhir/service/PemasokService.kt
@@ -0,0 +1,34 @@
+package com.example.pam_tugasakhir.service
+
+import com.example.pam_tugasakhir.model.Pemasok
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface PemasokService {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    @POST("insertpemasok.php")
+    suspend fun insertPemasok(@Body pemasok: Pemasok)
+
+    @GET("readpemasok.php")
+    suspend fun getAllPemasok(): List<Pemasok>
+
+    @GET("detailpemasok.php")
+    suspend fun getPemasokById(@Query("id_pemasok") idPemasok: String): Pemasok
+
+    @PUT("editpemasok.php")
+    suspend fun updatePemasok(@Query("id_pemasok") idPemasok: String, @Body pemasok: Pemasok)
+
+    @DELETE("deletepemasok.php")
+    suspend fun deletePemasok(@Query("id_pemasok") idPemasok: String): Response<Void>
+}
