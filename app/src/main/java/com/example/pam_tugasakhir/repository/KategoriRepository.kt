957dae4cbbc429007cc3490d794c928cfe689de9
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-22 19:58:12 +0700

    menambah KategoriRepository

---
+++ app/src/main/java/com/example/pam_tugasakhir/repository/KategoriRepository.kt
@@ -0,0 +1,53 @@
+package com.example.pam_tugasakhir.repository
+
+import com.example.pam_tugasakhir.model.Kategori
+import com.example.pam_tugasakhir.service.KategoriService
+import okio.IOException
+
+interface KategoriRepository {
+    suspend fun insertKategori(kategori: Kategori)
+
+    suspend fun getKategori(): List<Kategori>
+
+    suspend fun updateKategori(idKategori: String, kategori: Kategori)
+
+    suspend fun deleteKategori(idKategori: String)
+
+    suspend fun getKategoriById(idKategori: String): Kategori
+}
+
+class NetworkKategoriRepository(
+    private val kategoriApiService: KategoriService
+) : KategoriRepository {
+    override suspend fun insertKategori(kategori: Kategori) {
+        kategoriApiService.insertKategori(kategori)
+    }
+
+    override suspend fun updateKategori(idKategori: String, kategori: Kategori) {
+        kategoriApiService.updateKategori(idKategori, kategori)
+    }
+
+    override suspend fun deleteKategori(idKategori: String) {
+        try {
+            val response = kategoriApiService.deleteKategori(idKategori)
+            if (!response.isSuccessful) {
+                throw IOException(
+                    "Failed to delete kategori. HTTP Status Code: " +
+                            "${response.code()}"
+                )
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getKategori(): List<Kategori> =
+        kategoriApiService.getAllKategori()
+
+    override suspend fun getKategoriById(idKategori: String): Kategori {
+        return kategoriApiService.getKategoriById(idKategori)
+    }
+}
