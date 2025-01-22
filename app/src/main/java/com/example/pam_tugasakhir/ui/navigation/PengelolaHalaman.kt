5e830f12cd83e649fd989d41d3bdd7ab004d3365
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-22 11:04:09 +0700

    menambah DestinasiMerkHome

--- app/src/main/java/com/example/pam_tugasakhir/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_tugasakhir/ui/navigation/PengelolaHalaman.kt
@@ -8,8 +8,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.pam_tugasakhir.ui.view.Merk.DestinasiMerkHome
+import com.example.pam_tugasakhir.ui.view.Merk.HomeMerkScreen
+import com.example.pam_tugasakhir.ui.view.pemasok.DestinasiPemasokDetail
 import com.example.pam_tugasakhir.ui.view.pemasok.DestinasiPemasokEntry
 import com.example.pam_tugasakhir.ui.view.pemasok.DestinasiPemasokHome
+import com.example.pam_tugasakhir.ui.view.pemasok.DetailPemasokScreen
 import com.example.pam_tugasakhir.ui.view.pemasok.EntryPemasokScreen
 import com.example.pam_tugasakhir.ui.view.pemasok.HomePemasokScreen
 import com.example.pam_tugasakhir.ui.view.produk.DestinasiDetail
@@ -32,6 +36,8 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 navigateToPemasok = { navController.navigate(DestinasiPemasokHome.route) },
                 onPemasokClick = { navController.navigate(DestinasiPemasokHome.route) },
+                navigateToMerk = { navController.navigate(DestinasiMerkHome.route) },
+                onMerkClick = { navController.navigate(DestinasiMerkHome.route) },
                 onDetailClick = { idProduk ->
                     navController.navigate("${DestinasiDetail.route}/$idProduk")
                 }
@@ -70,9 +76,11 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                 navigateToPemasokEntry = {
                     navController.navigate(DestinasiPemasokEntry.route)
                 },
-                onDetailClick = { //idPemasok ->
-                    //navController.navigate("${DestinasiPemasokDetail.route}/$idPemasok")
+                onDetailClick = { idPemasok ->
+                    navController.navigate("${DestinasiPemasokDetail.route}/$idPemasok")
                 },
+                navigateToMerk = { navController.navigate(DestinasiMerkHome.route) },
+                onMerkClick = { navController.navigate(DestinasiMerkHome.route) },
                 onBack = {
                     navController.navigate(DestinasiHome.route)
                 },
@@ -91,17 +99,45 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
             )
         }
 
-       /* composable(
+        composable(
             route = "${DestinasiPemasokDetail.route}/{idPemasok}",
             arguments = listOf(navArgument("idPemasok") { type = NavType.StringType })
         ) { backStackEntry ->
             val idPemasok = backStackEntry.arguments?.getString("idPemasok") ?: ""
             DetailPemasokScreen(
                 idPemasok = idPemasok,
-                onNavigateBack = { navController.navigateUp() },
+                onNavigateBack = { navController.popBackStack() },
                 onEditClick = {
-                    navController.navigate("${DestinasiPemasokEntry.route}?idPemasok=$idPemasok")
+                    //navController.navigate("${DestinasiPemasokUpdate.route}?idPemasok=$idPemasok")
                 }
-            )*/
+            )
+        }
+
+        composable(DestinasiMerkHome.route) {
+            HomeMerkScreen(
+                navigateToMerkEntry = {
+                    //navController.navigate(DestinasiMerkEntry.route)
+                },
+                onDetailClick = { idMerk ->
+                    //navController.navigate("${DestinasiMerkDetail.route}/$idMerk")
+                },
+                onBack = {
+                    navController.navigate(DestinasiHome.route)
+                },
+            )
+        }
+
     }
 }
+        /*omposable(
+            route = "${DestinasiPemasokUpdate.route}/{idPemasok}"
+        ) { backStackEntry ->
+            val idPemasok = backStackEntry.arguments?.getString("idPemasok") ?: ""
+            UpdatePemasokScreen(
+                idPemasok = idPemasok,
+                onNavigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
+*/
