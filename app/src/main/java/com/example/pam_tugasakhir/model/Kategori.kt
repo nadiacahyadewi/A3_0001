eef0a9479853d127238cb089fc7a1173e138113d
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-22 19:57:43 +0700

    menambah dataclass Kategori

---
+++ app/src/main/java/com/example/pam_tugasakhir/model/Kategori.kt
@@ -0,0 +1,17 @@
+package com.example.pam_tugasakhir.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Kategori(
+    @SerialName("id_kategori")
+    val idKategori: String,
+
+    @SerialName("nama_kategori")
+    val namaKategori: String,
+
+    @SerialName("deskripsi_kategori")
+    val deskripsiKategori: String
+)
+
