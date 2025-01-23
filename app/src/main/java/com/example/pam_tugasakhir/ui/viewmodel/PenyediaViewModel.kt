e7159e5ecaf3a5ea9aac95df91d145bda512be8f
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-23 15:34:34 +0700

    menambah UpdateProdukVM

--- app/src/main/java/com/example/pam_tugasakhir/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam_tugasakhir/ui/viewmodel/PenyediaViewModel.kt
@@ -24,12 +24,16 @@ object PenyediaViewModel {
         initializer { ProdukHomeVM(aplikasiproduk().container.produkRepository) }
         initializer { ProdukInsertVM(aplikasiproduk().container.produkRepository) }
         initializer { ProdukDetailVM(aplikasiproduk().container.produkRepository) }
+        initializer { UpdateProdukVM(aplikasiproduk().container.produkRepository) }
+
         initializer { PemasokHomeVM(aplikasiproduk().container.pemasokRepository) }
         initializer { PemasokInsertVM(aplikasiproduk().container.pemasokRepository) }
         initializer { PemasokDetailVM(aplikasiproduk().container.pemasokRepository) }
+
         initializer { MerkHomeVM(aplikasiproduk().container.merkRepository) }
         initializer { MerkInsertVM(aplikasiproduk().container.merkRepository) }
         initializer { MerkDetailVM(aplikasiproduk().container.merkRepository) }
+
         initializer { KategoriHomeVM(aplikasiproduk().container.kategoriRepository) }
         initializer { KategoriInsertVM(aplikasiproduk().container.kategoriRepository) }
         initializer { KategoriDetailVM(aplikasiproduk().container.kategoriRepository) }
